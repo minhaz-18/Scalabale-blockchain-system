4fff29f30fce002781381db576c8e181828696b5
minhaz-18 <minhaz.mahmud.18@gmail.com>
2022-09-25 02:53:13 +0600

    initial folder and venv install

---
+++ src/folder_structure_01.py
@@ -0,0 +1,100 @@
+import folder_structure as fs
+
+# project/
+fs.project()
+
+# project/src/user_end
+fs.src_user_end()
+
+# project/src/miner_end
+fs.src_miner_end()
+
+# project/src/miner_end/p2p
+fs.src_p2p()
+
+# project/src/miner_end/ipfs
+fs.src_ipfs()
+
+# project/src/miner_end/tx_verification
+fs.src_tx_verification()
+
+# project/src/miner_end/block_generation
+fs.src_block_generation()
+
+# project/src/miner_end/reverse_block_verification
+fs.src_reverse_block_verification()
+
+# project/output
+fs.output()
+
+# project/output/user_end
+fs.output_user_end()
+
+# project/output/user_end/key_pairs
+fs.output_key_pairs()
+
+# project/output/user_end/addresses
+fs.output_addresses()
+
+# project/output/user_end/tx
+fs.output_tx()
+
+# project/output/user_end/tx_cid
+fs.output_tx_cid()
+
+# project/output/miner_end
+fs.output_miner_end()
+
+# project/output/miner_end/p2p
+fs.output_p2p()
+
+# project/output/miner_end/p2p/receiving_block_cid
+fs.p2p_receiving_block_cid()
+
+# project/output/miner_end/p2p/receiving_tx_cid
+fs.p2p_receiving_tx_cid()
+
+# project/output/miner_end/p2p/sending_tx_cid
+fs.p2p_sending_tx_cid()
+
+# project/output/miner_end/p2p/sending_block_cid
+fs.p2p_sending_block_cid()
+
+# project/output/miner_end/ipfs
+fs.output_ipfs()
+
+# project/output/miner_end/ipfs/raw_block_sending
+fs.ipfs_raw_block_sending()
+
+# project/output/miner_end/ipfs/raw_block_fetching
+fs.ipfs_raw_block_fetching()
+
+# project/output/miner_end/ipfs/raw_tx_sending
+fs.ipfs_raw_tx_sending()
+
+# project/output/miner_end/ipfs/raw_tx_fetching
+fs.ipfs_raw_tx_fetching()
+
+# project/output/miner_end/verified_tx
+fs.output_verified_tx()
+
+# project/output/miner_end/verified_tx/mempool
+fs.verified_tx_mempool()
+
+# project/output/miner_end/verified_tx/used_mempool
+fs.verified_tx_used_mempool()
+
+# project/output/miner_end/verified_tx/used_raw_tx_fetched_from_ipfs
+fs.verified_tx_used_raw_tx_fetched_from_ipfs()
+
+# project/output/miner_end/raw_block
+fs.output_raw_block()
+
+# project/output/miner_end/raw_block_cid
+fs.output_raw_block_cid()
+
+# project/output/miner_end/hash_block
+fs.output_hash_block()
+
+# project/output/miner_end/receiver_address_list
+fs.output_receiver_address_list()
