466f2f2e33a97d7b74018f8745cd7ad8b326d4ab
faisalalamapu1 <faisalalamapu1@gmail.com>
2022-09-28 17:46:32 +0600

    absolute path

--- src/user_end/key_pairs_address_generation.py
+++ src/user_end/key_pairs_address_generation.py
@@ -7,9 +7,8 @@ import sys
 import time
 import hashlib
 
-code_dir = os.getcwd()
-os.chdir("..")
-os.chdir("src")
+code_dir = os.getcwdb()
+os.chdir("E:\BlockChain\Scalabale-blockchain-system\src")
 src = os.getcwd()
 sys.path.insert(1, src)  # for importing folder_structure.py
 os.chdir(code_dir)  # returning to the code dir
@@ -17,11 +16,11 @@ from folder_structure import output_key_pairs, output_addresses
 
 
 def key_pairs_generation():
-    privKey = secrets.randbelow(generator_secp256k1.order())  # generated private key
+    privKey = secrets.randbelow(secp256k1_generator.order())  # generated private key
     private_key_hex = hex(privKey)  # ultimate private key
 
     # public key generation
-    pubKey = (generator_secp256k1 * privKey).pair()  # generated public key
+    pubKey = (secp256k1_generator * privKey).pair()  # generated public key
     public_key_hex_tuple = (hex(pubKey[0]), hex(pubKey[1]))
     public_key_hex = hex(pubKey[0]) + hex(pubKey[1])  # ultimate public key
     keys_dict = {"public_key": public_key_hex, "private_key": private_key_hex}
