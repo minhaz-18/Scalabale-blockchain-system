dce530d27776dc37dc20dd74b3c54cab29f1a181
minhaz-18 <minhaz.mahmud.18@gmail.com>
2022-10-25 20:58:21 +0600

    updated

--- src/user_end/my_own_p2p_application_json.py
+++ src/user_end/my_own_p2p_application_json.py
@@ -31,10 +31,10 @@ os.chdir(src_user_end())
 print("p2p_sending_tx_cid: ", p2p_sending_tx_cid())
 print("p2p_sending_block_cid: ", p2p_sending_block_cid())
 
-# node_1 = MyOwnPeer2PeerNode("192.168.0.209", 8001) ## Minhaz's Pc Local IP ##
+node_1 = MyOwnPeer2PeerNode("192.168.0.209", 8081) ## Minhaz's Pc Local IP ##
 # node_1 = MyOwnPeer2PeerNode("192.168.0.1", 8001) ## Minhaz's Pc Local gateway ##
 # node_1 = MyOwnPeer2PeerNode("144.48.162.18", 8001) ## Minhaz's pc pubic IP ##
-node_1 = MyOwnPeer2PeerNode("192.168.0.217", 8082) ## Minhaz's laptop Local IP ##
+# node_1 = MyOwnPeer2PeerNode("192.168.0.217", 8082) ## Minhaz's laptop Local IP ##
 # node_1 = MyOwnPeer2PeerNode("192.168.0.1", 8002) ## Minhaz's laptop Local gateway ##
 # node_1 = MyOwnPeer2PeerNode("144.48.162.18", 8001) ## Minhaz's laptop public IP ##
 # node_1 = MyOwnPeer2PeerNode("192.168.0.105", 8001) ## Opu's Local IP ##
@@ -58,7 +58,7 @@ time.sleep(1)
 # node_1.connect_with_node('192.168.0.1', 8001) ## Minhaz's PC gateway ip ##
 # node_1.connect_with_node('144.48.162.18', 8002) ## Minhaz's PC Public ip ##
 # node_1.connect_with_node('192.168.0.217', 8001) ## Minhaz's laptop Local ip ##
-node_1.connect_with_node('144.48.162.18', 8081) ## Minhaz's laptop Public ip ##
+node_1.connect_with_node('144.48.162.18', 8082) ## Minhaz's laptop Public ip ##
 # node_1.connect_with_node('192.168.0.1', 8002) ## Minhaz's laptop gateway ip ##
 
 #node_1.connect_with_node('127.0.0.1', 8002)
@@ -76,29 +76,33 @@ print("Sending at: " + str(time.time()))
 block_file_dir = p2p_sending_block_cid()  # this directory is only for sending files not the ultimate block directory
 block_ls = os.listdir(block_file_dir)
 if len(block_ls) != 0:
-    block_file_name = os.path.join(block_file_dir, block_ls[0])
-    with open(block_file_name) as file:
-        data = json.load(file)
-        node_1.send_to_nodes(data)
-        # os.remove(block_file_name)
-
+    for each_block in block_ls:
+        block_file_name = os.path.join(block_file_dir, each_block)
+        with open(block_file_name) as file:
+            data = json.load(file)
+            node_1.send_to_nodes(data)
+    for each_block_for_del in block_ls:
+        block_name_for_del = os.path.join(block_file_dir, each_block_for_del)
+        os.remove(block_name_for_del)
+# --------------------------------------------------------------------------------------------------------------------
 # For sending transaction ------------------------------------------------------------------------------
 tx_file_dir = output_tx_cid()
-ls = os.listdir(tx_file_dir)
-#print(ls)
-for file_num in ls:
-    file_name = os.path.join(tx_file_dir, file_num)
-    with open(file_name) as file:
-        data = json.load(file)
-        node_1.send_to_nodes(data)
-        # os.remove(file_name)
+tx_ls = os.listdir(tx_file_dir)
+if len(tx_ls) != 0:
+    #print(ls)
+    for file_num in tx_ls:
+        file_name = os.path.join(tx_file_dir, file_num)
+        with open(file_name) as file:
+            data = json.load(file)
+            node_1.send_to_nodes(data)
+    for file_num_for_del in tx_ls:
+        file_name_for_del = os.path.join(tx_file_dir, file_num_for_del)
+        os.remove(file_name_for_del)
 
 # ------------------------------------------------------------------------------------------------------
 
 
 
-
-# --------------------------------------------------------------------------------------------------------------------
 
 
 
